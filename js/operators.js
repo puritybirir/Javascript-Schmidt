e924cb0a777e9bb5cf16792c11e8aa42d2c675bf
puritybirir <puritybirir@gmail.com>
2019-10-29 09:08:34 +0300

    Operators

---
+++ js/operators.js
@@ -0,0 +1,29 @@
+/****
+ * Basic Operators
+ */
+
+var year, yearJohn, yearMark;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now/10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
