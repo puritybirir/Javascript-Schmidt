e375b7246c6d290da30dba819a1c7468d986620d
puritybirir <puritybirir@gmail.com>
2020-02-18 11:35:39 +0300

    The this keyword

---
+++ js/hoistingScoping/hoisting.js
@@ -0,0 +1,18 @@
+/**
+ * Hoisting
+ */
+
+
+ // Functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2019-year);
+};
+
+// Variables
+
+
+
+
+
---
+++ js/scoping/scoping.js
@@ -0,0 +1,17 @@
+var a = 'Hello';
+first()
+
+function first () {
+    var b = 'Hi!';
+    second()
+
+    function second(){
+        var c= 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a, d);
+}
