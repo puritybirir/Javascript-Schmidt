92582f4a2268085eeef7f750d0e3c955abc753fa
puritybirir <puritybirir@gmail.com>
2020-02-19 15:52:38 +0300

    Event listener

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -8,3 +8,46 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    // We need a random number
+    var dice = Math.floor(Math.random()*6) +1;
+
+    // We need to display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src= 'dice-' + dice + '.png'
+
+    //Update the round score if the rolled number is not a 1
+
+});
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML='<em>' + dice + '<em>'
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+
+
+
+
+
+
+
+
